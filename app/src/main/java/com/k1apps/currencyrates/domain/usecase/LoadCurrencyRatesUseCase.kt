0ff9e4ba45e7ae99389d375361db432592e5fab3
kayvan <kayvan.goli@gmail.com>
2023-10-01 14:44:14 +0330

    Handle sad path in LoadCurrencyRatesUseCase ### -Create ConnectionManager abstraction -Create CurrencyRatesException -Follow Red-Green-Refactor TDD cycle

--- app/src/main/java/com/k1apps/currencyrates/domain/usecase/LoadCurrencyRatesUseCase.kt
+++ app/src/main/java/com/k1apps/currencyrates/domain/usecase/LoadCurrencyRatesUseCase.kt
@@ -7,11 +7,29 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.isActive
 import kotlin.time.Duration.Companion.minutes
 
-class LoadCurrencyRatesUseCase(private val repository: CurrencyRateRepository) {
+class LoadCurrencyRatesUseCase(
+    private val repository: CurrencyRateRepository,
+    private val connectionManager: ConnectionManager
+) {
     operator fun invoke(): Flow<Result<List<CurrencyRateData>>> = flow {
         while (currentCoroutineContext().isActive) {
-            repository.getCurrencyRates()
+            if (connectionManager.isOnline().not()) {
+                emit(getErrorResult(CurrencyRatesException.Error.NO_CONNECTIVITY))
+            } else {
+                val repoResult = repository.getCurrencyRates()
+                if (isResultValid(repoResult)) {
+                    emit(Result.success(arrayListOf()))
+                } else {
+                    emit(getErrorResult(CurrencyRatesException.Error.INVALID_DATA))
+                }
+            }
             delay(2.minutes)
         }
     }
+
+    private fun isResultValid(result: Result<List<CurrencyRateData>>) =
+        result.getOrNull()?.isNotEmpty() ?: false
+
+    private fun getErrorResult(error: CurrencyRatesException.Error) =
+        Result.failure<List<CurrencyRateData>>(CurrencyRatesException(error))
 }
