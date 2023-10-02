84cced0203422e5754189055ca07105d7b6cc96d
kayvan <kayvan.goli@gmail.com>
2023-10-02 20:56:51 +0330

    Add runtime-livedata dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.1")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation ("io.mockk:mockk:1.13.4")
